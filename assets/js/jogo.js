96aefe91641b5281ec5245475a30e60590774cf8
Gabriel Estéfono <gabrielestefono@hotmail.com>
2022-11-17 11:50:42 -0300

    sexto commit

--- assets/js/jogo.js
+++ assets/js/jogo.js
@@ -2,57 +2,67 @@ var secao = document.getElementById('conteudo')
 var papel = document.getElementById('papel')
 var tesoura = document.getElementById('tesoura')
 var pedra = document.getElementById('pedra')
+var botaoperdeu = '<div class="play-again"><p class="play-again__text">Você perdeu!</p><button class="play-again__button" id="recomecar">Tentar novamente</button></div>'
+var botaoganhou = '<div class="play-again"><p class="play-again__text">Você Ganhou!</p><button class="play-again__button" id="recomecar">Tentar novamente</button></div>'
+var botaoempatou = '<div class="play-again"><p class="play-again__text">Empate!</p><button class="play-again__button" id="recomecar">Tentar novamente</button></div>'
+
+function cliqueipapel(){
+    secao.className = 'conteudo__2'
+    var escolher = Math.round(Math.random() * 10)
+    if(escolher % 3 == 0){
+        secao.innerHTML = papel.outerHTML + botaoperdeu + tesoura.outerHTML
+    }
+    if(escolher % 3 == 1){
+        secao.innerHTML = papel.outerHTML + botaoganhou + pedra.outerHTML
+    }
+    if(escolher % 3 == 2){
+        secao.innerHTML = papel.outerHTML + botaoempatou + papel.outerHTML
+    }
+}
+
+function cliqueitesoura(){
+    secao.className = 'conteudo__2'
+    var escolher = Math.round(Math.random() * 10)
+    if(escolher % 3 == 0){
+        secao.innerHTML = tesoura.outerHTML + botaoempatou + tesoura.outerHTML
+    }
+    if(escolher % 3 == 1){
+        secao.innerHTML = tesoura.outerHTML + botaoperdeu + pedra.outerHTML
+    }
+    if(escolher % 3 == 2){
+        secao.innerHTML = tesoura.outerHTML + botaoganhou + papel.outerHTML
+    }
+}
+
+function cliqueipedra(){
+    secao.className = 'conteudo__2'
+    var escolher = Math.round(Math.random() * 10)
+    if(escolher % 3 == 0){
+        secao.innerHTML = pedra.outerHTML + botaoganhou + tesoura.outerHTML
+    }
+    if(escolher % 3 == 1){
+        secao.innerHTML = pedra.outerHTML + botaoempatou + pedra.outerHTML
+    }
+    if(escolher % 3 == 2){
+        secao.innerHTML = pedra.outerHTML + botaoperdeu + papel.outerHTML
+    }
+}
+
+function voltaraonormal(){
+    secao.className = 'conteudo__1'
+    secao.innerHTML = papel.outerHTML + pedra.outerHTML + tesoura.outerHTML
+}
+
+
+function botaocriado(){
+    var recomecar = document.getElementById('recomecar')
+    recomecar.addEventListener('click', voltaraonormal)
+}
 
 papel.addEventListener('click', ()=>{
-    tesoura.remove()
-    pedra.remove()
-    var escolher = Math.round(Math.random() * 10) 
-    if (escolher % 3 == 0){
-        papel.remove()
-        secao.innerHTML = papel.outerHTML + tesoura.outerHTML
-    }
-    else if (escolher % 3 == 1){
-        papel.remove()
-        secao.innerHTML = papel.outerHTML + papel.outerHTML
-    }
-    else if (escolher % 3 == 2){
-        papel.remove()
-        secao.innerHTML = papel.outerHTML + pedra.outerHTML
-    }
+    cliqueipapel()
+    botaocriado()
 })
 
-tesoura.addEventListener('click', ()=>{
-    papel.remove()
-    pedra.remove()
-    var escolher = Math.round(Math.random() * 10) 
-    if (escolher % 3 == 0){
-        tesoura.remove()
-        secao.innerHTML = tesoura.outerHTML + tesoura.outerHTML
-    }
-    else if (escolher % 3 == 1){
-        tesoura.remove()
-        secao.innerHTML = tesoura.outerHTML + papel.outerHTML
-    }
-    else if (escolher % 3 == 2){
-        tesoura.remove()
-        secao.innerHTML = tesoura.outerHTML + pedra.outerHTML
-    }
-})
-
-pedra.addEventListener('click', ()=>{
-    tesoura.remove()
-    papel.remove()
-    var escolher = Math.round(Math.random() * 10) 
-    if (escolher % 3 == 0){
-        pedra.remove()
-        secao.innerHTML = pedra.outerHTML + tesoura.outerHTML
-    }
-    else if (escolher % 3 == 1){
-        pedra.remove()
-        secao.innerHTML = pedra.outerHTML + papel.outerHTML
-    }
-    else if (escolher % 3 == 2){
-        papel.remove()
-        secao.innerHTML = pedra.outerHTML + pedra.outerHTML
-    }
-})
+tesoura.addEventListener('click', cliqueitesoura)
+pedra.addEventListener('click', cliqueipedra)
